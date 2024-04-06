3ee39610e0c49fdaa490b90f295dad585b29c258
webazex <webazex@gmail.com>
2024-04-06 10:12:16 +0300

    included supports, assets, menu files

--- wp-content/themes/atheme/functions.php
+++ wp-content/themes/atheme/functions.php
@@ -1,6 +1,11 @@
 <?php
 use Webazex\App\App as App;
-use Webazex\App\Core\Core as Core;
 require_once get_template_directory().'/App/autoload.php';
-Core::refreshWhiteListDir();
-App::getAppDirectory();
+//menu
+require_once App::getIncludesDirectory().'registeredMenuArea.php';
+
+//theme supports
+require_once App::getIncludesDirectory().'supports.php';
+
+//assets
+require_once App::getIncludesDirectory().'sources.php';
