28b10099202678f741dd8dc9d7b16d0db31f0c5b
webazex <webazex@gmail.com>
2024-04-07 13:38:21 +0300

    add footer menu

--- wp-content/themes/atheme/footer.php
+++ wp-content/themes/atheme/footer.php
@@ -1,7 +1,15 @@
 <footer class="footer">
 	<div class="site-size">
 		<div class="site-size__footer-container">
-
+			<?php
+				wp_nav_menu( [
+					'theme_location'  => 'footer',
+					'echo'            => true,
+					'container' => false,
+					'depth' => 1,
+					'fallback_cb' => '__return_empty_string'
+				] );
+			?>
 		</div>
 	</div>
 </footer>
