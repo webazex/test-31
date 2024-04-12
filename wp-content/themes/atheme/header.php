f685a6be4eb14221b04ced26c02a35a905173597
webazex <webazex@gmail.com>
2024-04-12 11:25:39 +0300

    add adaptive header

--- wp-content/themes/atheme/header.php
+++ wp-content/themes/atheme/header.php
@@ -6,6 +6,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Lunasima:wght@400;700&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
 </head>
 <body>
@@ -13,6 +14,11 @@
 	<div class="site-size">
 		<div class="site-size__header-container">
 			<div class="header-container__header-menu">
+				<div class="header-menu__mobile-btn">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
 				<?php
 					wp_nav_menu( [
 						'theme_location'  => 'header',
--- wp-content/themes/atheme/src/js/main.js
+++ wp-content/themes/atheme/src/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function (){
-
+    $('.header-menu__mobile-btn').click(function (e){
+        if ($(this).hasClass('open')){
+            $(this).removeClass('open');
+            $('.header-container__header-menu ul.menu').removeClass('active');
+        }else{
+            $(this).addClass('open');
+            $('.header-container__header-menu ul.menu').addClass('active');
+        }
+    });
 });
