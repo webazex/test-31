2c9334d411f35da757c580f841891ad5d80055ef
webazex <webazex@gmail.com>
2024-04-16 12:19:45 +0300

    add default page template

--- wp-content/themes/atheme/page.php
+++ wp-content/themes/atheme/page.php
@@ -1,1 +1,12 @@
 <?php
+get_header();
+?>
+<main>
+	<div class="site-size">
+		<div class="site-size__container-page">
+			<?php the_content();?>
+		</div>
+	</div>
+
+</main>
+<?php get_footer(); ?>
